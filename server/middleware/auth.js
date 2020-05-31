9e7edfda8bb03ab2a36b1a8cb3eaccfa1b6ce29a
georgekara13 <george_kara_13@hotmail.com>
2020-05-31 14:22:57 +0300

    Log error in auth middleware instead of throwing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,10 +1,14 @@
-const {User} = require('./../model/user')
+const {User}     = require('./../model/user')
+const { logger } = require('../configuration/logger')
 
 let auth = (req, res, next) => {
   let token = req.cookies.auth
 
   User.findByToken(token, (err, user) => {
-    if (err) throw err
+    if (err){
+      logger.warn(err)
+      return res.status(400).send(err)
+    }
     if (!user) return res.json({error:true})
 
     req.token = token
