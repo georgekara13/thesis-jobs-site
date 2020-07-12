1532958fb49f2145102b8b83997ade45185e8420
georgekara13 <george_kara_13@hotmail.com>
2020-07-12 14:39:32 +0300

    Update addsource script

--- scraper/scripts/addsource.js
+++ scraper/scripts/addsource.js
@@ -1,22 +1,25 @@
 //script for adding sources to the source collection
 const axios         = require('axios')
 const { readStdin } = require('../core/Parser/readstdin')
-const { Conf }      = require('../core/Classes/conf')
+const scraperConf   = require('../configuration/environment/scraperconf').scraperConf()
 
-const {conf_path} = readStdin()
-const confFile    = new Conf(conf_path)
+async function addSource() {
+  const content = await readStdin()
 
-const content = confFile.getContent()
+  console.log(`Fetched content: ${JSON.stringify(content)}\n`)
 
-console.log(`Fetched content from '${conf_path}': ${JSON.stringify(content)}\n`)
+  axios.post(`${scraperConf.HOST}/api/addsource`, content)
+       .then(response => {
+         console.log(`Source '${content.name}' is now scrapable!`)
+         console.log(response.data)
+         process.exit()
+       })
+       .catch(err => {
+         console.log(err)
+         process.exit()
+       })
+}
 
-axios.post('http://localhost:3001/api/addsource', content)
-     .then(response => {
-       console.log(`Source '${content.name}' is now scrapable!`)
-       console.log(response.data)
-       process.exit()
-     })
-     .catch(err => {
-       console.log(err)
-       process.exit()
-     })
+//TODO ADD checks if source already exists
+
+addSource()
