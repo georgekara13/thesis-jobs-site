c95e98bb110f1e724b6ac035ad545dd35cdd6849
George Karagiannis <george_kara_13@hotmail.com>
2020-08-08 13:35:21 +0300

    SCRP-02: Add scrape for weworkremotely.com (#3)

---
+++ scraper/functions/WeWorkRemotely.js
@@ -0,0 +1,51 @@
+const webdriver = require('selenium-webdriver')
+const saltedMd5 = require('salted-md5')
+const salt      = '0Yy LUm@o'
+
+
+module.exports =
+{
+  results_page: async function(ad_urls,driver)
+  {
+    const fetched_urls = await driver.findElements(webdriver.By.xpath("/html/body/div[3]/div/section/article/ul/li/a"))
+    for ( let i = 0; i < fetched_urls.length; i++ )
+    {
+      ad_urls.push(await fetched_urls[i].getAttribute('href'))
+      //ad_urls[i] = ad_urls[i].replace(/^/, 'https://weworkremotely.com')
+    }
+  },
+
+  ad_page: async function(driver,ad_url,ad_fields)
+  {
+    ad_fields.location = "Remote"
+    ad_fields.jobTag   = ["it"]
+    ad_fields.url      = ad_url
+    ad_fields.id       = saltedMd5(ad_url, salt)
+
+    //Title
+    driver.wait(
+      webdriver.until.elementLocated(webdriver.By.css(".listing-header-container")),
+      200000
+    )
+    .then(element => {
+      return driver.wait(
+        webdriver.until.elementIsVisible(element),
+          200000
+        )
+    })
+
+    await driver.findElement(webdriver.By.xpath("/html/body/div[3]/div/div[1]/h1")).getText()
+                .then((title) => {ad_fields.title = title})
+                .catch(err => {console.log(err.message)})
+
+    await driver.findElement(webdriver.By.xpath("/html/body/div[3]/div/div[2]/h2/a")).getText()
+                .then((company) => {ad_fields.company = company})
+                .catch(err => {console.log(err.message)})
+
+    await driver.findElement(webdriver.By.id("job-listing-show-container")).getText()
+                .then((description) => {ad_fields.description = description})
+                .catch(err => {console.log(err.message)})
+
+    return ad_fields
+  }
+}
