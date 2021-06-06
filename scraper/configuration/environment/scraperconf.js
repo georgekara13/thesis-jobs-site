62ee3f1256de9949b75e9dfffcf409f2f3c00331
georgekara13 <george_kara_13@hotmail.com>
2021-06-06 22:17:18 +0300

    Scraper container - TODO fix api request errors

--- scraper/configuration/environment/scraperconf.js
+++ scraper/configuration/environment/scraperconf.js
@@ -1,13 +1,14 @@
 const scraperConf = () => {
   //MONGODB_URI will be a dockerfile env variable
-  return process.env.MODE == 'production' ? {
-                                              HOST: process.env.HOST,
-                                              SECRET: process.env.SECRET
-                                            }
-                                          : {
-                                              HOST: 'http://localhost:3001',
-                                              SECRET: 'dev_env_testpass123'
-                                            }
+  return process.env.MODE == 'production'
+    ? {
+        HOST: 'http://localhost:3001',
+        SECRET: 'dev_env_testpass123',
+      }
+    : {
+        HOST: 'http://localhost:3001',
+        SECRET: 'dev_env_testpass123',
+      }
 }
 
 module.exports = { scraperConf }
